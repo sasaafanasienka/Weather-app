427d22afd03fa471fc0ca21b573a45babb766379
sasaafanasienka <mark0182@tut.by>
2021-08-20 15:55:09 +0300

    try to set heroku settings

--- app.js
+++ app.js
@@ -2,13 +2,16 @@ const express =  require('express')
 const config = require('config')
 const mongoose = require('mongoose')
 
+require('dotenv').config()
+const cors = require('cors')
+
 const app = express()
 
+const PORT = config.get('port') || 5000 
+app.use(cors())
 app.use(express.json({ extended: true }))
-
 app.use('/api/auth',  require('./routes/auth.routes'))
 
-const PORT = config.get('port') || 5000
 
 async function start() {
     try {
