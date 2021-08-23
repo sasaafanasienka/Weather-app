dc8b0cb0c02eb1d95dc795b5be18b30643bc656f
sasaafanasienka <mark0182@tut.by>
2021-08-23 23:31:29 +0300

    settings

--- client/src/components/Alert/Alert.js
+++ client/src/components/Alert/Alert.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import MiniButton from '../buttons/MiniButton/MiniButton'
 import './Alert.sass'
-import closeIcon from '../../images/close.svg'
 import smile from '../../images/smile.svg'
 import sad from '../../images/sad.svg'
 import surprise from '../../images/surprise.svg'
@@ -15,7 +13,6 @@ const Alert = props => {
             {options.type === 'success' && <img className='Alert__icon' src={smile}></img>}
             {options.type === 'error' && <img className='Alert__icon' src={sad}></img>}
             <p>{message}</p>
-            {/* <MiniButton onClick={close} icon={closeIcon} style={{position: 'absolute', top: '6px', right: '18px'}}/> */}
         </div>
     );
 }
