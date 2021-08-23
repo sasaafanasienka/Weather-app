60b75d299481f91390b43bf4b3ad365403710481
sasaafanasienka <mark0182@tut.by>
2021-08-23 18:24:56 +0300

    make it better

--- index.js
+++ index.js
@@ -7,17 +7,11 @@ const cors = require('cors')
 
 const app = express()
 
-const PORT = config.get('port') || 5000 
+const PORT = process.env.PORT || 5000
+
 app.use(cors())
 app.use(express.json({ extended: true }))
 app.use('/api/auth',  require('./routes/auth.routes'))
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-});
 
 async function start() {
     try {
@@ -26,7 +20,7 @@ async function start() {
             useUnifiedTopology: true,
             useCreateIndex: true
         }) 
-        app.listen(PORT, () => { console.log(`app has been started on port ${PORT}...`)})
+        app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
     } catch (e) {
         console.log('Server error', e.message)
         process.exit(1)
